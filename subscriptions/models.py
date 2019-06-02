f4fe351388664f2dbef70ea8ac403451f0a7304e
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-06-03 03:58:52 +0700

    [master] add subscription model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Subscription(models.Model):
+    """
+    Subscription model:
+    """
+    email = models.EmailField()
+    query = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.email}: {self.query}'
+
+    def __repr__(self):
+        return f'{self.email}: {self.query}'
+
+
